5a464e439cadb85bbdb726661cb8d08aae0aad94
Jozef Cajkovic <jozef.cajkovic@gmail.com>
2023-01-13 15:31:33 +0100

    fix(gh,tfm): PIXEL-11 fix workflows (#3)

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,18 +1,7 @@
-# terraform {
-#   cloud {
-#     organization = "pixel-systems"
-#     hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
-
-#     workspaces {
-#       name = "pixel-systems-gh-repositories"
-#     }
-
-#   }
-# }
 
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-311008325132" #core-cicd
+    bucket  = "terraform-state-311008325132" #pixel-systems-org account
     region  = "eu-west-1"
     encrypt = true
 
